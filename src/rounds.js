d629711c95b4d6850e5ca5b89a40fd17a9c90851
Jon Higgins <jon@jonhiggins.co.uk>
2018-06-21 17:24:33 +1000

    add resolver to get all fixtures in tournament  and add "rounds" text to fixtures schema

---
+++ src/rounds.js
@@ -0,0 +1,12 @@
+/* Map API's 'matchday' to a string representing current round of competition */
+
+module.exports = {
+  1: 'Group stage: match 1',
+  2: 'Group stage: match 2',
+  3: 'Group stage: match 3',
+  4: 'Round of 16',
+  5: 'Quarter finals',
+  6: 'Semi finals',
+  7: 'Third place',
+  8: 'Final'
+}
--- utils/requests.js
+++ utils/requests.js
@@ -2,4 +2,5 @@ const get = require('./axios')
 
 module.exports.getPlayers = (teamID) => get(`teams/${teamID}/players`)
 module.exports.getGames = (teamID) => get(`teams/${teamID}/fixtures`)
+module.exports.getFixtures = () => get('competitions/467/fixtures')
 module.exports.getTeams = () => get('competitions/467/teams')
